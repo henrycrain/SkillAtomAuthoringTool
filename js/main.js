4280eb828105f9012b4d362ca114551a1f10bdb2
Henry Crain <henry.crain@stonybrook.edu>
2021-06-14 12:45:56 -0400

    New branch to test drag-and-drop interface. Added test image that can be dragged.

--- js/main.js
+++ js/main.js
@@ -15,3 +15,45 @@ $('.create').click(function () {
 $('.cancel').click(function () {
   $(this).parents('.modal-bg').css('display', 'none');
 });
+
+let canvasJq = $('.main');  // jQuery wrapper for canvas
+let canvas = canvasJq[0];  // The actual canvas
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let ctx = canvas.getContext('2d');
+let img = new Image();
+img.src = "tile.png";
+img.onload = function () {
+  ctx.drawImage(img, 0, 0);
+};
+
+let canvasOffset = canvasJq.offset();
+let canvasMouseX;
+let canvasMouseY;
+let isDragging;
+
+function startDrag(event) {
+  canvasMouseX = event.clientX - canvasOffset.left;
+  canvasMouseY = event.clientY - canvasOffset.top;
+  isDragging = true;
+}
+
+function drag(event) {
+  canvasMouseX = event.clientX - canvasOffset.left;
+  canvasMouseY = event.clientY - canvasOffset.top;
+  if (isDragging) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.drawImage(img, canvasMouseX, canvasMouseY);
+  }
+}
+
+function stopDrag(event) {
+  canvasMouseX = event.clientX - canvasOffset.left;
+  canvasMouseY = event.clientY - canvasOffset.top;
+  isDragging = false;
+}
+
+canvasJq.mousedown(startDrag);
+canvasJq.mousemove(drag);
+canvasJq.mouseup(stopDrag);
