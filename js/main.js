4ca5cb213b69b691170793b88cf4a92ea37de1ed
Henry Crain <henry.crain@stonybrook.edu>
2021-06-15 12:24:30 -0400

    Improved dragging images onto the canvas

--- js/main.js
+++ js/main.js
@@ -21,29 +21,64 @@ let canvas = canvasJq[0];  // The actual canvas
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
+let atomsOnCanvas = [];
+
 let ctx = canvas.getContext('2d');
-
-let images = [
-  { obj: new Image(), src: "tile.png", left: 0, top:0, right:0, bottom: 0, width: 0, height: 0, isDragging: false },
-  { obj: new Image(), src: "tile-wide.png", left: 100, top: 100, right: 0, bottom: 0, width: 0, height: 0, isDragging: false }
-];
-
-for (let img of images) {
-  img.obj.src = img.src;
-  img.obj.onload = function () {
-    ctx.drawImage(img.obj, img.left, img.top);
-    img.width = img.obj.width;
-    img.right = img.left + img.width;
-    img.height = img.obj.height;
-    img.bottom = img.top + img.height;
-  };
-}
 
 let canvasOffset = canvasJq.offset();
 let lastX;
 let lastY;
 let dragging;
 
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  for (let img of atomsOnCanvas) {
+    ctx.drawImage(img.obj, img.left, img.top);
+  }
+}
+
+function dragAtom(event) {
+  let coords = event.target.getBoundingClientRect();
+  let offsetX = event.clientX - coords.x;
+  let offsetY = event.clientY - coords.y;
+  // Proportional offsets because atoms are bigger on the canvas than in the list
+  let offset = { left: offsetX / event.target.width, top: offsetY / event.target.height };
+  let offsetStr = JSON.stringify(offset);
+  event.originalEvent.dataTransfer.setData('text/uri-list', event.target.src);
+  event.originalEvent.dataTransfer.setData('application/json', offsetStr);
+}
+
+function addAtom(event) {
+  event.preventDefault();
+  let src = event.originalEvent.dataTransfer.getData('text/uri-list');
+  let offsetStr = event.originalEvent.dataTransfer.getData('application/json');
+  let offset = JSON.parse(offsetStr);
+
+  let newAtom = {
+    obj: new Image(),  // The actual image object
+    src: src,
+    isDragging: false
+  };
+
+  newAtom.obj.src = newAtom.src;
+  newAtom.obj.onload = function () {
+    newAtom.width = newAtom.obj.width;
+    newAtom.height = newAtom.obj.height;
+    newAtom.left = event.clientX - canvasOffset.left - offset.left * newAtom.width;
+    newAtom.top = event.clientY - canvasOffset.top - offset.top * newAtom.height;
+    newAtom.right = newAtom.left + newAtom.width;
+    newAtom.bottom = newAtom.top + newAtom.height;
+    draw();
+  };
+  atomsOnCanvas.push(newAtom);
+}
+
+$('#atoms').children().on('dragstart', dragAtom);
+canvasJq.on('dragover', event => event.preventDefault());
+canvasJq.on('drop', addAtom);
+
+// TODO Currently, dragging overlapping objects drags all of them, which might not be expected behavior
+// TODO To change this, I think we would need to assign z-indices to the images
 function startDrag(event) {
   event.preventDefault();
   event.stopPropagation();
@@ -52,7 +87,7 @@ function startDrag(event) {
   let mouseY = event.clientY - canvasOffset.top;
 
   dragging = false;
-  for (let img of images) {
+  for (let img of atomsOnCanvas) {
     if (mouseX >= img.left && mouseX <= img.right &&
         mouseY >= img.top && mouseY <= img.bottom) {
       dragging = true;
@@ -74,7 +109,7 @@ function drag(event) {
     let movementX = mouseX - lastX;
     let movementY = mouseY - lastY;
 
-    for (let img of images) {
+    for (let img of atomsOnCanvas) {
       if (img.isDragging) {
         img.left += movementX;
         img.right += movementX;
@@ -95,15 +130,8 @@ function stopDrag(event) {
   event.stopPropagation();
 
   dragging = false;
-  for (let img of images) {
+  for (let img of atomsOnCanvas) {
     img.isDragging = false;
-  }
-}
-
-function draw() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  for (let img of images) {
-    ctx.drawImage(img.obj, img.left, img.top);
   }
 }
 
